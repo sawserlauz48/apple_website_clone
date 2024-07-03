537eb0e31d43bb4bac5d37ab60b4c19a1584e397
sawserlauz48 <sawserlauz48@gmail.com>
2024-07-03 15:23:57 +0300

    finishing the 3d modle

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -9,6 +9,9 @@ import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
 
+import { animateWithGsapTimeline } from "../utils/animations";
+
+
 const Model = () => {
     const [size, setSize] = useState('small');
     const [model, setModel] = useState({
@@ -31,10 +34,10 @@ const tl =gsap.timeline();
 
 useEffect(()=>{
   if(size === "large"){
-    
+    animateWithGsapTimeline(tl,small,smallRotation,"#view1","#view2",{transform : "translateX(-100%)",duration : 2})
   }
   if(size === "small"){
-
+    animateWithGsapTimeline(tl,large,largeRotation,"#view2","#view1",{transform : "translateX(0%)",duration : 2})
   }
 },[size])
 useGSAP(() => {
