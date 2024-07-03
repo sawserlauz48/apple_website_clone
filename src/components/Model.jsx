dc3a6b6e9c6c828b63598558af745dbadd461007
sawserlauz48 <sawserlauz48@gmail.com>
2024-07-03 11:06:17 +0300

    THREE instal and setup

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,48 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import ModelView from "./ModelView"
+import { useRef, useState } from "react"
+import { yellowImg } from "../utils"
+
+import * as THREE from "three"
 
 const Model = () => {
+ const [size, setSize] = useState("small")
+ const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8f8a81","#ffe7b9","#6f6c64"],
+    img: yellowImg
+ })
+
+ // camera contorl for the model view
+ const cameraControlSmall = useRef();
+ const cameraControlLarge = useRef();
+// models
+ const small = useRef(new THREE.Group);
+ const large = useRef(new THREE.Group);
+//rotation
+const [smallRotation, setSmallRotation] = useState(0)
+const [largeRotation, setLargeRotation] = useState(0)
+
+
+    useGSAP(()=>{
+        gsap.to("#heading", {
+            y: 0,
+            opacity : 1           
+        }
+        )
+    },[])
   return (
-    <div>Model</div>
+    <section className="common-padding">
+        <div className="screen-max-width">
+            <h1 id="heading" className="section-heading">Take a closer look.</h1>
+            <div className="flex flex-col items-center mt-5">
+                <div className=" w-full h-[75vh] md:h-[90vh] overflow-hidden relative ">
+                    <ModelView />
+                </div>
+            </div>
+        </div>
+        </section>
   )
 }
 
