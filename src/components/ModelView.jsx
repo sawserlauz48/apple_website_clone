061e6f7ea9f5648b14b51c3638e99ca1fea1f589
sawserlauz48 <sawserlauz48@gmail.com>
2024-07-03 13:02:35 +0300

    minor bug fix

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,15 +1,23 @@
 import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
 import Lights from "./Lights";
 import { Suspense } from "react";
-import IPhone from './IPhone';
+import IPhone from "./IPhone";
 import * as THREE from "three";
 
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationState, size, item}) => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  size,
+  item,
+}) => {
   return (
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
@@ -17,19 +25,23 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotationState, siz
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
       <Lights />
-      
-      <OrbitControls 
+
+      <OrbitControls
         makeDefault
         ref={controlRef}
         enableZoom={false}
         enablePan={false}
         rotateSpeed={0.4}
-        target={new THREE.Vector3(0, 0 ,0)}
+        target={new THREE.Vector3(0, 0, 0)}
         onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
-      /> 
-      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
-        <Suspense fallback={<Loader />}>
-          <IPhone 
+      />
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<>LOADING</>}>
+          <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
             size={size}
@@ -37,7 +49,7 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotationState, siz
         </Suspense>
       </group>
     </View>
-  )
-}
+  );
+};
 
-export default ModelView
+export default ModelView;
